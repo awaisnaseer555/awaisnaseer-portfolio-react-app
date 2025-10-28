d9e96d6f0a47848c5cd9f77a9fa57fc7f0a46910
Awais Naseer <awaisnaseer555@gmail.com>
2025-10-28 17:06:35 +0500

    meta data improved

--- app/layout.js
+++ app/layout.js
@@ -7,11 +7,29 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
+import { type } from "os";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Portfolio of Awais Naseer - Principal Software Engineer",
-  description: "Portfolio site of Awais Naseer, a self-taught full-stack developer with a passion for continuous learning. I enjoy exploring new technologies, collaborating with others, and taking on challenging projects. As a quick learner, I’m always eager to grow and push my limits."
+  description: "Portfolio site of Awais Naseer, a self-taught full-stack developer with a passion for continuous learning. I enjoy exploring new technologies, collaborating with others, and taking on challenging projects. As a quick learner, I’m always eager to grow and push my limits.",
+  siteName: "Awais Naseer Portfolio",
+  authors: [{ name: "Awais Naseer", url: "https://awaisnaseer.vercel.com" }],
+  keywords: [
+    "Awais Naseer",
+    "Awais",  "Awais Naseer Portfolio",
+    "Portfolio",
+    "Full-Stack Developer",
+    "Software Engineer",
+    "Web Developer",
+    "JavaScript"],
+  type: "website",
+  metadataBase: [new URL("https://awaisnaseer.vercel.app")],
+  images: [
+    {
+      url: "/self.jpg",
+      alt: "Awais Naseer Portfolio Image",
+    }]
 };
 
 export default function RootLayout({ children }) {
